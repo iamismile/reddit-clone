c9e86eb1bbaaed4e5798b111ffc3bd1709c1a2b9
Ismile Hossain <iamismile.dev@gmail.com>
2023-02-03 17:38:49 +0600

    user creation

--- src/components/Modal/Auth/Signup.tsx
+++ src/components/Modal/Auth/Signup.tsx
@@ -1,6 +1,8 @@
+import { auth } from '@/firebase/clientApp';
 import { useAuthModalActions } from '@/store/useAuthModalStore';
 import { Button, Flex, Input, Text } from '@chakra-ui/react';
 import React, { useState } from 'react';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 
 const Signup: React.FC = () => {
   const { setView } = useAuthModalActions();
@@ -9,6 +11,9 @@ const Signup: React.FC = () => {
     password: '',
     confirmPassword: '',
   });
+  const [error, setError] = useState('');
+  const [createUserWithEmailAndPassword, user, loading, userError] =
+    useCreateUserWithEmailAndPassword(auth);
 
   const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSignupForm((prevState) => ({
@@ -17,7 +22,15 @@ const Signup: React.FC = () => {
     }));
   };
 
-  const onFormSubmit = () => {};
+  const onFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (error) setError('');
+    if (signupForm.password !== signupForm.confirmPassword) {
+      setError('Passwords do not match');
+      return;
+    }
+    createUserWithEmailAndPassword(signupForm.email, signupForm.password);
+  };
 
   return (
     <form onSubmit={onFormSubmit}>
@@ -87,7 +100,12 @@ const Signup: React.FC = () => {
         }}
         bg="gray.50"
       />
-      <Button type="submit" width="100%" height="36px" mt={2} mb={2}>
+      {error && (
+        <Text fontSize="10pt" textAlign="center" color="red">
+          {error}
+        </Text>
+      )}
+      <Button type="submit" isLoading={loading} width="100%" height="36px" mt={2} mb={2}>
         Sign Up
       </Button>
 
